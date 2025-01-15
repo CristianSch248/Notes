cadfe618797e262952baad9faa7ad4a724ff5b8a
cristian Schmitzhaus <cristianschmitzhaus2000@gmail.com>
2025-01-15 15:44:22 -0300

    Ajustes na formatação

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
-const notesRoutes = require('./routes/notes');
-const notesHandlers = require('./handlers/noteHandlers');
-const db = require('./db/connection');
+const notesRoutes = require('./routes/notes')
+const notesHandlers = require('./handlers/noteHandlers')
+const db = require('./db/connection')
 
-const app = express();
-const port = 8000;
+const app = express()
+const port = 8000
 
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/notes', notesRoutes);
-app.get('/', notesHandlers.renderNotesHome);
+app.use('/notes', notesRoutes)
+app.get('/', notesHandlers.renderNotesHome)
 
 db.initDb((err) => {
   if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
+    console.error('Erro ao conectar ao banco de dados:', err)
   } else {
     app.listen(port, () => {
-      console.log(`Servidor rodando na porta: ${port}`);
-    });
+      console.log(`Servidor rodando na porta: ${port}`)
+    })
   }
-});
+})
